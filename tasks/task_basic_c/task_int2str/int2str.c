50ee61df541ae32f43433cf5376ddc145a4f42ac
radmila <radmila.pustova@gmail.com>
2024-11-02 00:07:57 +0000

    prohodit testy

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,45 @@
 #include <assert.h>
 #include "int2str.h"
 #include "stdio.h"
+#include <stdlib.h>
 
 char* int2str(int number) {
-    char* str = "0";
-    return str;
+    int num2 = number;
+    int len = 0;
+    char* answ = malloc(12);
+    if(number == 2147483647) {
+        return("2147483647");
+    }
+    if(number == -2147483648) {
+        return("-2147483648");
+    }
+    if(number == 0) {
+        return("0");
+    }
+    if(number == -0) {
+        return("-0");
+    }
+    if(number < 0) {
+        *answ = '-';
+        answ++;
+        len++;
+        number *= -1;
+        num2 *= -1;
+    }
+
+    while (number !=0) {
+        number /= 10;
+        answ++;
+        len++;
+    }
+    *answ = '\0';
+
+    while (num2 != 0) {
+        answ--;
+        *answ = num2%10 + '0';
+        num2 /= 10;
+
+    }
+    answ--;
+    return answ;
 }
